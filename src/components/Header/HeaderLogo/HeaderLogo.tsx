f2c387ac690bf6247ceb29acc0f4f8fd68e23543
Ramazan <ozturkiramazan@gmail.com>
2022-01-16 15:44:36 +0300

    responsive header and logo

--- src/components/Header/HeaderLogo/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo/HeaderLogo.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './header-logo.style.scss';
 
 import logo from '../../../images/logo.svg';
+import shortLogo from '../../../images/short-logo.svg';
 
 const HeaderLogo = () => {
   return (
     <>
       <div className="header_logo">
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" className="header_logo_long" />
+        <img src={shortLogo} alt="Short Logo" className="header_logo_short" />
       </div>
     </>
   );
--- src/components/Header/HeaderNav/HeaderNav.tsx
+++ src/components/Header/HeaderNav/HeaderNav.tsx
@@ -5,29 +5,32 @@ const HeaderNav = () => {
   return (
     <>
       <div className="nav">
-        <div className="nav_link">
-          <a href="#">About Us</a>
+        <div className="nav_wrapper">
+          <div className="nav_link">
+            <a href="#">About Us</a>
+          </div>
+          <div className="nav_link">
+            <a href="#">How it works</a>
+          </div>
+          <div className="nav_link">
+            <a href="#">Become a Partner</a>
+          </div>
+          <div className="nav_link">
+            <a href="#">EUR</a>
+            <div className="nav_link_arrow-icon"></div>
+          </div>
+          <div className="nav_link">
+            <a href="#">EN</a>
+            <div className="nav_link_arrow-icon"></div>
+          </div>
+          <div className="nav_link">
+            <a href="#">Sign In</a>
+          </div>
+          <div className="nav_link">
+            <a href="#">Get Access</a>
+          </div>
         </div>
-        <div className="nav_link">
-          <a href="#">How it works</a>
-        </div>
-        <div className="nav_link">
-          <a href="#">Become a Partner</a>
-        </div>
-        <div className="nav_link">
-          <a href="#">EUR</a>
-          <div className="nav_link_arrow-icon"></div>
-        </div>
-        <div className="nav_link">
-          <a href="#">EN</a>
-          <div className="nav_link_arrow-icon"></div>
-        </div>
-        <div className="nav_link">
-          <a href="#">Sign In</a>
-        </div>
-        <div className="nav_link">
-          <a href="#">Get Access</a>
-        </div>
+        <div className="nav_burger"></div>
       </div>
     </>
   );
